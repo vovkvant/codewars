226618a04f299fd1edabbc9142bc6ef3f4fedd00
vovkvant <vovkvant@gmail.com>
2020-08-23 00:18:28 +0300

    Added binarytreetopologies

---
+++ src/main/java/algoexpert/veryhard/binarytreetopologies/Program.java
@@ -0,0 +1,29 @@
+package algoexpert.veryhard.binarytreetopologies;
+
+class Program {
+    public static void main(String args[]) {
+        System.out.println(numberOfBinaryTreeTopologies(7));
+    }
+
+    public static int numberOfBinaryTreeTopologies(int n) {
+        // Write your code here.
+        if (n == 0 || n == 1) return 1;
+        if (n == 2) return 2;
+
+        n--;
+        int result = 0;
+        for (int i = 0; i <= n; i++) {
+            int leftResult = 1;
+            int rightResult = 1;
+            if(n - i>=0) {
+                leftResult = numberOfBinaryTreeTopologies(n - i);
+            }
+            if(i>= 0) {
+                rightResult = numberOfBinaryTreeTopologies(i);
+            }
+            result = result + leftResult * rightResult;
+
+        }
+        return result;
+    }
+}
